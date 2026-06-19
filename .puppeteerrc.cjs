ffb852b9ff7bc0ed9aaff40a66755735621fb284
romario-developer <romariotecla@icloud.com>
2026-06-19 08:47:41 -0300

    correção de erros

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -1,9 +1,6 @@
-const {join} = require('path');
+const { join } = require('path');
 
-/**
- * @type {import("puppeteer").Configuration}
- */
 module.exports = {
-  // Configura a pasta de cache do Puppeteer para ficar dentro do projeto
-  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+  // Define o caminho absoluto para o cache do Puppeteer no Render
+  cacheDirectory: join('/opt/render/project/src', '.cache', 'puppeteer'),
 };
